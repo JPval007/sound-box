942e7888151b28243481b60463ac40580ee96963
Juan Pablo <juanvalenzuela4business@gmail.com>
2023-11-10 14:37:29 -0600

    The sound works Just missing the key press feature

--- src/components/PlayKey.js
+++ src/components/PlayKey.js
@@ -1,11 +1,23 @@
 //Import Audio libraries
+import {Howl} from 'howler';
 
 
 function PlayKey(props) {
+
+    // Method that plays the sound
+    const PlaySound = (src)=> {
+        // Set up the howl object with new custom properties
+        const sound = new Howl({
+            src,
+            html5 : true
+        });
+        //Actually play the sound
+        sound.play();
+    };
     
     return (
         <>
-            <button className="btn btn-primary drum-pad w-100" id={props.text}>{props.text}
+            <button className="btn btn-primary drum-pad w-100" id={props.text} onClick={()=>(PlaySound(props.source))} >{props.text}
             <audio className="clip" id={props.text} src={props.source}></audio></button>
         </>
     );
